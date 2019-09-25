83f461bee31deb3db7f270bc56d013fad8ef3a7c
ruchaa31 <52658309+ruchaa31@users.noreply.github.com>
2019-09-25 14:28:10 +0530

    Add files via upload

---
+++ json.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+df= pd.read_json(r"parsed.json")
+print(df)
+df.to_csv("NewCSV.csv")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
